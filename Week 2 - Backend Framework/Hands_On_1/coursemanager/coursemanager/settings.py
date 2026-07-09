ec7a6c10fd07c259896fe22e830c57a1c935dd16
dev-girii <giridharan.r.2023.cce@ritchennai.edu.in>
2026-07-09 10:51:05 +0530

    Added: Hands On 3 - Task 1

--- Week 2 - Backend Framework/Hands_On_1/coursemanager/coursemanager/settings.py
+++ Week 2 - Backend Framework/Hands_On_1/coursemanager/coursemanager/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'courses.apps.CoursesConfig'
+    'courses.apps.CoursesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
